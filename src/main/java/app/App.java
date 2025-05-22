b12b125eb8c908b609a532a12fb7669419615f0e
BNenzo <enzoletona06@gmail.com>
2025-05-22 12:35:39 -0300

    feat: Add initial project configuration

---
+++ src/main/java/app/App.java
@@ -0,0 +1,42 @@
+package app;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Inicio del programa");
+
+        // create a CharStream that reads from file
+        CharStream input = CharStreams.fromFileName("src/programa.txt");
+
+        // create a lexer that feeds off of input CharStream
+        idLexer lexer = new idLexer(input);
+
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // create a parser that feeds off the tokens buffer
+        idParser parser = new idParser(tokens);
+
+        // create Listener
+        // idBaseListener escucha = new MiListener(parser);
+        // parser.addParseListener(escucha);
+
+        // Solicito al parser que comience indicando una regla gramatical
+        // En este caso la regla es el simbolo inicial
+        // parser.s();
+        System.out.println("");
+        ParseTree tree = parser.s();
+        System.out.println("");
+        System.out.println(tree.toStringTree(parser));
+        System.out.println("fin del programa");
+    }
+}
---
+++ src/main/java/app/idBaseListener.java
@@ -0,0 +1,54 @@
+// Generated from /Users/enzoletona/Desktop/TC/proyecto-final-tc/src/main/java/app/id.g4 by ANTLR 4.13.1
+
+package app;
+
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link idListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class idBaseListener implements idListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterS(idParser.SContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitS(idParser.SContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
